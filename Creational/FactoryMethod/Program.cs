3db597310ada079612910958d9f04f7d17ab5859
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-11 13:51:52 +0300

    Add project files.

---
+++ Creational/FactoryMethod/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ *  Bir nesneyi oluştururken o nesnenin sorumlu olduğu başka nesneleri de oluşturmak istiyorsunuz. Bu ihtiyacınızı nasıl giderirsiniz.
+ * 
+ */
