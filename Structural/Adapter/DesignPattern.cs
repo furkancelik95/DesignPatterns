fe53a7ea1265ea9b8458cebecb51a00da278fd02
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-12 13:28:08 +0300

    Structural Patterns introduction with Adapter Pattern

---
+++ Structural/Adapter/DesignPattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    /*
+     * Senaryo:
+     * Bir dış kaynaktan ürünlerinizi çekiyorsunuz.
+     * Bu dış kaynak size XML formatında veri gönderiyor.
+     * Fakat sizin List<Product>'a ihtiyacınız var.
+     */
+
+    public class Product
+    {
+
+    }
+
+    // 1. Adım: İstemcinin erişeceği Adaptör interface'i oluşturulur.
+    public interface IProductCollectionAdapter
+    {
+        List<Product> GetProductsFromSource();
+    }
+
+    // 2. Adım: Adapte edilecek kaynak:
+    public class XmlSource : IProductCollectionAdapter
+    {
+        public List<Product> GetProductsFromSource()
+        {
+            Console.WriteLine("XML verisi, objeye dönüştürüldü");
+            return new List<Product>();
+        }
+    }
+
+    public class ProductListAdapter : IProductCollectionAdapter
+    {
+        private readonly XmlSource source;
+
+        public ProductListAdapter(XmlSource source)
+        {
+            this.source = source;
+        }
+
+        public List<Product> GetProductsFromSource()
+        {
+            return source.GetProductsFromSource();
+        }
+    }
+
+}
---
+++ Structural/Adapter/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * Uygulamanız belirli bir türde veri bekliyor, ancak veri formatı bambaşka bir türde.
+ * İhtiyacınız olan veriyi bu dış kaynaktan nasıl alırsınız?
+ */
