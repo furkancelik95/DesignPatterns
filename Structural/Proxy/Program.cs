e52100be4608ff071c54557ebf10869a144f7c91
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-12 16:41:49 +0300

    Proxy Pattern done.

---
+++ Structural/Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy;
+
+Console.WriteLine("Hello, World!");
+
+/*
+ * Maliyetli bir nesneniz var her isteği doğrudan bu nesneye göndermek istemiyorsunuz. Nasıl çözersiniz?
+ */
+
+MathProxy math = new MathProxy(new Proxy.Math());
+Console.WriteLine(math.Division(10, 2));
+Console.WriteLine(math.Division(10, 0));
