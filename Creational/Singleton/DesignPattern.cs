2e08d3f57a954cbc1ed96bbfd1808730871b8a42
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-12 11:44:24 +0300

    Creational folder organized and Singleton Pattern done.

---
+++ Creational/Singleton/DesignPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /*
+     * Senaryo:
+     * Mail konfigürasyonunun sadece bir instance olması yeterli.
+     */
+
+    public class MailConfigurator
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSSL { get; set; }
+
+        private MailConfigurator()
+        {
+            // MailConfigurator sınıfından instance oluşturulmasını engellemek için private constructor kullanıyoruz.
+        }
+
+        private static MailConfigurator instance;
+        public static MailConfigurator CreateInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MailConfigurator();
+            }
+            return instance;
+        }
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * Uygulamanızda SADECE 1 adet olmasını istediğiniz bir instance var. Bunu nasıl sağlarsınız?
+ */
+MailConfigurator mailConfigurator = MailConfigurator.CreateInstance();
+mailConfigurator.Host = "smtp.gmail.com";
+
+MailConfigurator mailConfigurator2 = MailConfigurator.CreateInstance();
+mailConfigurator2.Host = "smtp.yandex.com";
+Console.WriteLine(mailConfigurator2.Host);
