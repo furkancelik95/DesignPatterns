19455f73a01a4fb34517331d6b3e6fcb40ee48ac
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-12 15:31:40 +0300

    Decorator Pattern done.

---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+using System.IO.Compression;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+Stream fStream = new FileStream("deneme.txt", FileMode.Create);
+Stream mStream = new MemoryStream();
+//Stream nStream = new NetworkStream(new TcpClient().Client);
+
+GZipStream stream = new GZipStream(fStream, CompressionLevel.Optimal);
+CryptoStream cryptoStream = new CryptoStream(stream, null, CryptoStreamMode.Write);
+
+/*
+ * Problem:
+ * Hali hazırda bellekte bulunan bir nesneye yeni özellik ya da fonksiyon kazandırmak isterseniz, nasıl bir yol izlersiniz?
+ */
+
+Mail mail = new Mail();
+SignedDecorator signedMail = new SignedDecorator(mail, "Furkan Enes Çelik");
+CryptedDecorator cryptedMail = new CryptedDecorator(signedMail);
+cryptedMail.Send();
