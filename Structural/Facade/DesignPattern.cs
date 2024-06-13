25a1e3043ed2a16c3b3e40a905dcef390da11039
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-13 10:15:39 +0300

    Facade Pattern done.

---
+++ Structural/Facade/DesignPattern.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+
+    public class OrderService
+    {
+        public void CreateOrder(string customerName, List<CartItem> cartItems)
+        {
+            Customer customer = new Customer { Name = customerName };
+            Order order = new Order { Id = 1453, Customer = customer, OrderDate = DateTime.Now };
+
+            var orderItems = cartItems.Select(ci => new OrderItem
+            {
+                OrderId = order.Id,
+                ProductId = ci.ProductId,
+                Quantity = ci.Quantity
+            }).ToList();
+
+            orderItems.ForEach(orderItem =>
+            {
+                order.OrderItems.Add(orderItem);
+                Console.WriteLine($"{customer.Name} isimli müşteri, {order.OrderDate} tarihinde, {orderItem.ProductId} id'li üründen, {orderItem.Quantity} adet sipariş verdi.");
+                Console.WriteLine($"ürünün stoğundan {orderItem.Quantity} kadar düşüldü");
+            });
+
+        }
+    }
+
+}
---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Facade;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * Çok fazla nesne kullanarak çok karmaşık işler yapmanız gereken bir senaryonuz var.
+ * Geliştiricinin en kolay biçimde bu işlemi yapması için ne  yapmalısınız?
+ */
+
+OrderService orderService = new OrderService();
+var cartItems = new List<CartItem>
+{
+    new CartItem { ProductId = 1, Quantity = 8 },
+    new CartItem { ProductId = 2, Quantity = 5 }
+};
+orderService.CreateOrder("Furkan", cartItems);
