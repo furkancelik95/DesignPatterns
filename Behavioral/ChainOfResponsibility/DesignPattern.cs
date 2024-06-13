14746a2c5ecfb8355be0ee8152cbf456a8212498
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-13 11:30:45 +0300

    Chain of Responsibility Pattern done.

---
+++ Behavioral/ChainOfResponsibility/DesignPattern.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    /*
+     * Senaryo:
+     * Bir otel rezervasyonu yapacaksınız. Bu istekte kurallar şöyle:
+     * 1 . Rezervasyon yapan kişi
+     * 2 . Check-in ve out tarihleri
+     * 3 . Otel müsait olmalı
+     * 4 . Doğru otele gitmeli
+     */
+
+    public class ReservationRequest
+    {
+        public string Owner { get; set; }
+        public DateOnly CheckIn { get; set; }
+        public DateOnly CheckOut { get; set; }
+        public int ParticipantsCount { get; set; }
+        public string Country { get; set; }
+    }
+
+    public interface IHandler
+    {
+        void Handler(ReservationRequest request);
+        IHandler Next { get; set; }
+    }
+
+    public abstract class HandlerBase : IHandler
+    {
+        public IHandler Next { get; set; }
+
+        public abstract void Handler(ReservationRequest request);
+        public HandlerBase()
+        {
+
+        }
+        public HandlerBase(HandlerBase next)
+        {
+            Next = next;
+        }
+    }
+
+    /*
+     * Senaryo:
+     * Bir otel rezervasyonu yapacaksınız. Bu istekte kurallar şöyle:
+     * 1 . Rezervasyon yapan kişi boş mu dolu mu
+     * 2 . Check-in ve out tarihleri
+     * 3 . Otel müsait olmalı
+     * 4 . Doğru otele gitmeli
+     */
+
+    public class CheckOwnerHandler : HandlerBase
+    {
+        public override void Handler(ReservationRequest reservationRequest)
+        {
+            if (string.IsNullOrEmpty(reservationRequest.Owner))
+            {
+                throw new Exception("Rezervasyon yapan kişi boş olamaz.");
+                return;
+            }
+            Next.Handler(reservationRequest);
+        }
+    }
+
+    public class CheckInAndOutHandler : HandlerBase
+    {
+        public override void Handler(ReservationRequest reservationRequest)
+        {
+            if (reservationRequest.CheckIn >= reservationRequest.CheckOut)
+            {
+                throw new Exception("Rezervasyon tarihleri hatalı");
+            }
+            Next.Handler(reservationRequest);
+        }
+    }
+
+    public class IsAvailableHandler : HandlerBase
+    {
+        public override void Handler(ReservationRequest reservationRequest)
+        {
+            var number = new Random().Next(0, 2);
+            var isEven = number % 2 == 0;
+            if (isEven)
+            {
+                throw new Exception("Ne yazık ki bu tarihlerde yer yok!");
+            }
+            Next.Handler(reservationRequest);
+        }
+    }
+
+    public class IsCountryTurkeyHandler : HandlerBase
+    {
+        public override void Handler(ReservationRequest reservationRequest)
+        {
+            if (reservationRequest.Country.Contains("Türkiye"))
+            {
+                Console.WriteLine("Türkiye'de rezervasyon yapıldı");
+            }
+            else
+            {
+                Next.Handler(reservationRequest);
+            }
+        }
+    }
+
+    public class IsCountryItalyHandler : HandlerBase
+    {
+        public override void Handler(ReservationRequest reservationRequest)
+        {
+            if (reservationRequest.Country.Contains("Italy"))
+            {
+                Console.WriteLine("İtalya'da rezervasyon yapıldı");
+            }
+            else
+            {
+                Next.Handler(reservationRequest);
+            }
+        }
+    }
+}
+
---
+++ Behavioral/ChainOfResponsibility/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ChainOfResponsibility;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * Bir istek birden fazla işlemden geçerek bir sonuca ulaşıyor. Her bir işlem benzer standartlar ve aynı amaç için çalışıyor.
+ * (Örnek: Asp.net Core'da http request pipeline ya da ATM'deki para analojisi).
+ * Hangi işlemin nasıl yapılacağını en ideal nasıl tasarlarsınız?
+ */
+
+ReservationRequest reservationRequest = new ReservationRequest
+{
+    Owner = "Furkan Enes Çelik",
+    CheckIn = new DateOnly(2024, 6, 18),
+    CheckOut = new DateOnly(2024, 6, 19),
+    Country = "Italy"
+};
+
+CheckOwnerHandler checkOwnerHandler = new CheckOwnerHandler();
+CheckInAndOutHandler checkInAndOutHandler = new CheckInAndOutHandler();
+IsAvailableHandler isAvailableHandler = new IsAvailableHandler();
+IsCountryTurkeyHandler isCountryTurkeyHandler = new IsCountryTurkeyHandler();
+IsCountryItalyHandler isCountryItalyHandler = new IsCountryItalyHandler();
+
+checkOwnerHandler.Next = checkInAndOutHandler;
+checkInAndOutHandler.Next = isAvailableHandler;
+isAvailableHandler.Next = isCountryTurkeyHandler;
+isCountryTurkeyHandler.Next = isCountryItalyHandler;
+
+checkOwnerHandler.Handler(reservationRequest);
+Console.WriteLine("Rezervasyon yapıldı");
