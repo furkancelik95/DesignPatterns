3a7a84f486a8cff88ce768b6467dbbfe079ca687
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-13 15:21:12 +0300

    Strategy Pattern done.

---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strategy;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * Bir iş için yazdığınız bir algoritma değişmesi gerekirse; minimum kod değişikliği ile bu güncellemeyi nasıl yaparsınız?
+ */
+
+ProductCollection productCollection = new ProductCollection();
+productCollection.Sort(new BubbleSortAlgorithm());
+productCollection.Sort(new HeapSortAlgorithm());
