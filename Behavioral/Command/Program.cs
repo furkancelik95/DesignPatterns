14b56d074b52e535d67405a93e4f14f48e240d82
furkancelik95 <furkancelik@sabanciuniv.edu>
2024-06-13 12:21:06 +0300

    Command Pattern done.

---
+++ Behavioral/Command/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Command;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * İstemciden sürekli olarak aksiyon alan (dosya işlemleri, db işlemleri, hesaplamalar vs.) bir uygulamada bu aksiyonları düzenli bir biçimde
+   yönetmek istiyorsanız, nasıl bir tasarım oluşturursunuz? 
+ */
+
+DatabaseCommandReceiver commandReceiver = new DatabaseCommandReceiver();
+DiscountProductCommand discountProductCommand = new DiscountProductCommand(8, 0.25m, commandReceiver);
+RaiseProductCommand raiseProductCommand = new RaiseProductCommand(16, 0.53m, commandReceiver);
+
+CommandInvoker invoker = new CommandInvoker();
+invoker.Add(discountProductCommand);
+invoker.Add(raiseProductCommand);
+invoker.Execute();
